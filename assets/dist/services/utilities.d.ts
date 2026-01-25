55f3c238e7d74cd6623a3bf9c7558f9d3a636bc9
Simon Asika <asika32764@gmail.com>
2026-01-25 22:15:05 +0800

    Release version: 0.2.3

--- assets/dist/services/utilities.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function prepareList<T = object>(items: T[], defaultData?: Record<string, any>, uidField?: string): ({
-    [x: string]: any;
-} & T)[];
-export declare function prepareListItem<T = object>(item: T, defaultData?: Record<string, any>, uidField?: string): {
-    [x: string]: any;
-} & T;
-export declare function deepClone<T>(item: T): T;
